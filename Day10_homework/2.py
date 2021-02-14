b382d8221397165ca3c617f84757231b3232a8b4
kssulko <katarzyna.m.sulkowska@gmail.com>
2021-02-14 20:46:37 +0100

    week2 homework

---
+++ Day10_homework/2.py
@@ -0,0 +1,51 @@
+import csv
+
+class Player:
+    def __init__(self, ID, name, score, age):
+        self.ID = ID
+        self.name = name
+        self.score = score
+        self.age = age
+
+    def __str__(self):
+        return f'{self.ID}, {self.name}, {self.score}, {self.age}'
+
+
+
+class PlayerList:
+    def __init__(self):
+        self.players = []
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def display_all(self):
+        for gamer in self.players:
+            print(gamer)
+
+    def sort(self, reverse=False):
+        self.players.sort(key=lambda player: player.score, reverse=True)
+
+    def from_csv(self, filename, delimiter=','):
+        with open(filename, 'r') as players_file:
+            reader = csv.DictReader(players_file, delimiter=delimiter)
+            for player_data in reader:
+                gamer = Player(
+                    player_data['ID'],
+                    player_data['name'],
+                    player_data['score'],
+                    player_data['age']
+                )
+                self.add_player(gamer)
+
+
+if __name__ == '__main__':
+
+
+    lista_graczy = PlayerList()
+    lista_graczy.from_csv('players.csv')
+    lista_graczy.display_all()
+
+    print('TOP 10 po sortowaniu')
+    lista_graczy.sort([:9])
+    lista_graczy.display_all()
---
+++ Day8_homework/3. Cezar.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, surname, pesel, age):
+        self.name = name
+        self.surname = surname
+        self.pesel = pesel
+        self.age = age
+
+if __name__== '__main__':
+    man = Person('Jan', 'Kowalski', 12344123, 40)
+    woman = Person('Janina', 'Kowalska', 8686867, )
+
+
